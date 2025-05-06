6f2b702e7a940800f7fb5a7e384c5025fee779f7
rstular <rok@stular.eu>
2025-05-06 17:04:29 +0200

    feat(auth): Add authentication

--- model_service/__init__.py
+++ model_service/__init__.py
@@ -2,13 +2,31 @@ import os
 
 from asgiref.wsgi import WsgiToAsgi
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
+from flask_httpauth import HTTPTokenAuth
 
 from model_service.dto import ModelServicePredictRequest, ModelServicePredictResponse
 from model_service.github import download_model
-from model_service.ml_model import get_model, load_model, get_model_version
+from model_service.ml_model import get_model, get_model_version, load_model
 
 load_dotenv()
+
+auth = HTTPTokenAuth(scheme="Bearer")
+
+token_file = os.getenv("AUTH_TOKEN_FILE")
+if not token_file:
+    raise ValueError("AUTH_TOKEN_FILE environment variable is not set.")
+with open(token_file, "r") as f:
+    STATIC_TOKEN = f.read().strip()
+
+
+@auth.verify_token
+def verify_token(token):
+    # Compare the provided token with our static token
+    if token == STATIC_TOKEN:
+        return True
+    return False
+
 
 # Download the model
 model_path, model_version = download_model(desired_version=os.getenv("MODEL_VERSION"))
@@ -18,6 +36,7 @@ app = Flask(__name__)
 
 
 @app.route("/predict", methods=["POST"])
+@auth.login_required
 def predict():
     # Parse the request data
     try:
@@ -44,11 +63,13 @@ def predict():
 
 
 @app.route("/version/model", methods=["GET"])
+@auth.login_required
 def version():
     return jsonify({"version": get_model_version()}), 200
 
 
 @app.route("/version/app", methods=["GET"])
+@auth.login_required
 def app_version():
     return jsonify({"version": os.getenv("SERVICE_VERSION")}), 200
 
